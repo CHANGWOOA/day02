c49a0917655b20001da1195cc23f2fafdf1fa373
changwoo <speed5312@naver.com>
2024-09-27 17:31:55 +0900

    셀프퀴즈까지 완료

--- src/if_/Ex03.java
+++ src/if_/Ex03.java
@@ -1,5 +1,7 @@
 package if_;
 
+import java.util.Scanner;
+
 public class Ex03 {
 public static void main(String[] args) {
 	int num=11;
@@ -9,5 +11,41 @@ public static void main(String[] args) {
 		System.out.println("else 실행");
 	}
 	System.out.println("다음 문장들 실행!!!");
+int n1, ret;
+int n2=10;
+//System.out.println(n1);
+//ret=n1+n2;
+
+if(n2>=10);
+n1=100;
+if(n2<10);
+else//if(n2<10)
+	n1=10;
+System.out.println(n1);
+
+Scanner input = new Scanner(System.in);
+int k=0, e=0, m=0, sum=0;
+System.out.println("국어점수 입력");
+k=input.nextInt();
+sum = k + e + m;
+
+int i=0;
+double d=0;
+char ch=0;
+String s =null;
+System.out.println(i);
+System.out.println(d);
+System.out.println(ch);
+System.out.println(s);
+if(d==0)
+	System.out.println("d에 입력하세요");
+if(s==null)
+	System.out.println("s에 입력하세요");
+
+
+
+
+
+
 }
 }
--- src/operator/Quiz01.java
+++ src/operator/Quiz01.java
@@ -5,7 +5,28 @@ import java.util.Scanner;
 public class Quiz01 {
 public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
+	System.out.println("수 입력 : ");
 	int num;
+	num=input.nextInt();
+	if(num%2==0){
+		System.out.println(num + " = 짝수 ");}
+	else {System.out.println(num + " = 홀수 ");}
+	
+	if(num%3==0)
+	{System.out.println(num + " = 3의 배수다");}
+	else {System.out.println(num + " = 3의 배수가 아니다");}
+	
+	System.out.print("두 수 입력 : ");
+	int num1;
+	num1=input.nextInt();
+	int num2;
+	num2=input.nextInt();
+	System.out.println("num1 : "+ num1 + ", num2 : "+num2);
+	if(num1<num2)
+	{System.out.println("num2가 num1보다 크다");}
+	
+	
+	
 	
 }
 }
