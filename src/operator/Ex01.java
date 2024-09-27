2daec64e8b58a78a063a2cb421aea8a4a27a3ced
changwoo <speed5312@naver.com>
2024-09-27 13:45:20 +0900

    if문 활용

--- src/operator/Ex01.java
+++ src/operator/Ex01.java
@@ -57,7 +57,33 @@ public static void main(String[] args) {
 	System.out.println(!true);
 	System.out.println(!(10>20));
 	
+	System.out.println("---증감 연산자");
+	n1=5;
+	++n1;
+	System.out.println("n1 : "+n1);
 	
+	n2=5;
+	n2++;
+	System.out.println("n2 : "+n2);
+	
+	System.out.println("------");
+	n1=10;
+	n2=++n1;
+	System.out.println("n1 : "+n1);
+	System.out.println("n2 : "+n2);
+	
+	System.out.println("------");
+	n1=10;
+	n2=n1++;//n2+=n1;
+	System.out.println("n1 : "+n1);
+	System.out.println("n2 : "+n2);
+	
+	System.out.println("---조건 연산자---");
+	n1=10; n2=20;
+	String r=(n1>n2)?"n1이 n2보다 크다" : "n2가 n1보다 크다";
+	System.out.println(r);
+	//(n1>n2 && n1%2 == 0)?"n1은n2보다 큰 짝수"
+	//System.out.println(n1%2==0);
 	
 	
 	
---
+++ src/operator/Quiz01.java
@@ -0,0 +1,11 @@
+package operator;
+
+import java.util.Scanner;
+
+public class Quiz01 {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	int num;
+	
+}
+}
